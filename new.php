16e7949ae755f7e30e9c9f984f07871f34525531
pidewap <pidewap@gmail.com>
2018-08-25 09:48:00 +0700

    Update new.php

--- new.php
+++ new.php
@@ -1,5 +1,6 @@
 <?php
     $mypix = simplexml_load_file('http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=100/xml');
+$mypix = preg_replace("/(<\/?)(\w+):([^>]*>)/", "$1$2$3", $mypix);
     foreach ($mypix->entry as $pixinfo):
         $title=$pixinfo->title;
         $image=$pixinfo->imimage[2];
