5357210f819985c6fb283a5f48856e2b377f2990
pidewap <pidewap@gmail.com>
2018-08-25 10:03:18 +0700

    Update new.php

--- new.php
+++ new.php
@@ -1,12 +1,6 @@
 <?php
 $xml = simplexml_load_file('http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=10/xml');
 foreach ($xml->entry as $val):
-echo '<a href="'.$val->id.'" title="'.$val->title.'"><img src="'.$val->imimage[2].'" alt="'.$val->title.'"></a>
-
-        
-        <div><a href="">Preview</div>
-
-        <div><strong><a href="'.$val->id.'" title="'.$val->title.'">'.$val->title.'</a></strong></div>
-        <div> from '.$val->imcollection->imname.'';
+echo '<a href="'.$val->id.'" title="'.$val->title.'"><img src="'.$val->im:image[2].'" alt="'.$val->title.'"></a>';
 endforeach;
 ?>
