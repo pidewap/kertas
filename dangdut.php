291a81faba15522dc3c84c808fc06094205d1656
pidewap <pidewap@gmail.com>
2018-08-25 11:20:52 +0700

    Create dangdut.php

---
+++ dangdut.php
@@ -0,0 +1,17 @@
+<?php
+$content=file_get_contents("https://itunes.apple.com/us/rss/topsongs/limit=100/genre=1274/json"); 
+$top_albums=json_decode($content);
+$tracks = $top_albums->feed->entry;
+echo '<textarea>{% set nav = {
+';
+foreach( $tracks as $track ) {
+$img = $track->{'im:image'}[0]->label;
+  $title = $track->{'im:name'}->label;
+  $artist = $track->{'im:artist'}->label;
+$n=rand(0,100000);
+echo ''.$n.': {title: "'.$title.'", artist: "'.$artist.'", img: "'.$img.'"},
+';
+}
+echo '
+} %}</textarea>';
+?>
