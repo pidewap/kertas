bab9e68fa8d83d64cd8d91f70d04ad13828ac622
pidewap <pidewap@gmail.com>
2018-08-25 10:19:43 +0700

    Update new.php

--- new.php
+++ new.php
@@ -1,7 +1,6 @@
 <?php
-$itunes_feed = 'https://itunes.apple.com/gb/rss/topsongs/limit=10/explicit=true/xml';
-$itunes_feed = file_get_contents($itunes_feed);
-$itunes_feed = preg_replace("/(</?)(w+)[^>]*>)/", "$1$2$3", $itunes_feed);
+$itunes_feed = file_get_contents('https://itunes.apple.com/gb/rss/topsongs/limit=10/explicit=true/xml');
+$itunes_feed = preg_replace('/(</?)(w+)[^>]*>)/', '$1$2$3', $itunes_feed);
 $itunes_xml = new SimpleXMLElement($itunes_feed);
 $itunes_entry = $itunes_xml->entry;
 foreach($itunes_entry as $entry){	     
