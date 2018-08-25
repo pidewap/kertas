3849c0583707770d79be6e089d3977607938a13e
pidewap <pidewap@gmail.com>
2018-08-25 10:22:42 +0700

    Update new.php

--- new.php
+++ new.php
@@ -1,10 +1,9 @@
 <?php
-$itunes_feed = file_get_contents('https://itunes.apple.com/gb/rss/topsongs/limit=10/explicit=true/xml');
-$itunes_feed = preg_replace('/(</?)(w+)[^>]*>)/', '$1$2$3', $itunes_feed);
-$itunes_xml = new SimpleXMLElement($itunes_feed);
-$itunes_entry = $itunes_xml->entry;
-foreach($itunes_entry as $entry){	     
-  $entry_id['im'] = $entry->id->attributes('im', TRUE);    
-  echo $entry_id['im']['id'];
+$content=file_get_contents("https://itunes.apple.com/us/rss/topalbums/limit=10/json"); 
+$top_albums=json_decode($content);
+$tracks = $top_albums->feed->entry;
+foreach( $tracks as $track ) {
+$image = $track->{'im:image'}[0]->label;
+echo $image . '<br />';
 }
 ?>
