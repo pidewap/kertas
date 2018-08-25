e514770ed85017252487017aa14fed6e93b8e249
pidewap <pidewap@gmail.com>
2018-08-25 09:56:53 +0700

    Update new.php

--- new.php
+++ new.php
@@ -1,21 +1,13 @@
 <?php
-$string = file_get_contents('http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=10/xml');
-$string = preg_replace("/(<\/?)(\w+):([^>]*>)/", "$1$2$3", $string);
-
-if ($f = @fopen($cache_file, 'w')) {
-    fwrite ($f, $string, strlen($string));
-    fclose($f);
-    }
-}
-$xml = simplexml_load_string($string);
-
+$xml = simplexml_load_file('http://api.flickr.com/services/feeds/photoset.gne?set=72157627229375826&nsid=73845487@N00&lang=en-us');
+$xml = preg_replace("/(<\/?)(\w+):([^>]*>)/", "$1$2$3", $xml);
 foreach ($xml->entry as $val):
 echo '<a href="'.$val->id.'" title="'.$val->title.'"><img src="'.$val->imimage[2].'" alt="'.$val->title.'"></a>
 
-        // .m4a preview url?
+        
         <div><a href="">Preview</div>
 
-        <div><strong>'.$count.'. <a href="'.$val->id.'" title="'.$val->title.'">'.$val->title.'</a></strong></div>
+        <div><strong><a href="'.$val->id.'" title="'.$val->title.'">'.$val->title.'</a></strong></div>
         <div> from '.$val->imcollection->imname.'';
 endforeach;
 ?>
