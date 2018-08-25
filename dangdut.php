6528bcc5c87a3fbc860ca229851d63f794a1424f
pidewap <pidewap@gmail.com>
2018-08-25 13:15:58 +0700

    Update dangdut.php

--- dangdut.php
+++ dangdut.php
@@ -1,5 +1,5 @@
 <?php
-$content=file_get_contents("https://itunes.apple.com/us/rss/topsongs/limit=100/genre=1274/json"); 
+$content=file_get_contents("https://itunes.apple.com/us/rss/topsongs/limit='.$_GET['page'].'/genre=1274/json"); 
 $top_albums=json_decode($content);
 $tracks = $top_albums->feed->entry;
 echo '<textarea>{% set nav = {
