5fbe7f3b2dab9258b6f18d24debb4f2a4dd85d45
pidewap <pidewap@gmail.com>
2018-08-25 10:25:55 +0700

    Update new.php

--- new.php
+++ new.php
@@ -1,9 +1,17 @@
 <?php
-$content=file_get_contents("https://itunes.apple.com/us/rss/topalbums/limit=10/json"); 
+$content=file_get_contents("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=100/json"); 
 $top_albums=json_decode($content);
 $tracks = $top_albums->feed->entry;
+echo '<textarea>{% set nav = {
+';
 foreach( $tracks as $track ) {
-$image = $track->{'im:image'}[0]->label;
-echo $image . '<br />';
+$img = $track->{'im:image'}[0]->label;
+  $title = $track->{'im:title'}[0]->label;
+  $artist = $track->{'im:artist'}[0]->label;
+$n=rand(0,100000);
+echo ''.$n.': {title: "'.$title.'", artist: "'.$artist.'", img: "'.$img.'"},
+';
 }
+echo '
+} %}</textarea>';
 ?>
