68a897fe67b6f61dcd9ef07c9a9ed2e1b371c72e
pidewap <pidewap@gmail.com>
2018-08-25 10:16:08 +0700

    Update new.php

--- new.php
+++ new.php
@@ -1,9 +1,12 @@
 <?php
-$itunes_xml = file_get_contents('https://itunes.apple.com/gb/rss/topsongs/limit=10/explicit=true/xml');
-  $itunes_xml = preg_replace("/(</?)(w+)[^>]*>)/", "$1$2$3", $itunes_xml);
-  $itunes_xml = simplexml_load_string($itunes_xml);
-  foreach ($itunes_xml->entry as $entry) {    
-  echo $entry->imprice['amount'];
-  }
-
+$itunes_feed = "https://itunes.apple.com/gb/rss/topsongs/limit=10/explicit=true/xml";
+$itunes_feed = file_get_contents($itunes_feed);
+$itunes_feed = preg_replace("/(</?)(w+)[^>]*>)/", "$1$2$3", $itunes_feed);
+$itunes_xml = new SimpleXMLElement($itunes_feed);
+$itunes_entry = $itunes_xml->entry;
+foreach($itunes_entry as $entry){	    
+  // Get the value of the entry ID, by using the 'im' namespace within the <id> attribute    
+  $entry_id['im'] = $entry->id->attributes('im', TRUE);    
+  echo $entry_id['im']['id'];
+}
 ?>
